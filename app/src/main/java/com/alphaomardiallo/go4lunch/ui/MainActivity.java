d2d7c1d4df901a9ad52177c8c41ec576ef75d235
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-04-08 17:04:05 +0200

    ViewBinding enabled

--- app/src/main/java/com/alphaomardiallo/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/alphaomardiallo/go4lunch/ui/MainActivity.java
@@ -1,16 +1,21 @@
 package com.alphaomardiallo.go4lunch.ui;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import com.alphaomardiallo.go4lunch.R;
+import com.alphaomardiallo.go4lunch.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
