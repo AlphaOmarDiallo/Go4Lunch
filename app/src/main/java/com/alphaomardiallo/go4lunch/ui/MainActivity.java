2ca6c4de1c3d7e18f7c04787a7aec1b20d6bd09e
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-04-13 13:35:35 +0200

    theme updated - email and google login method setup

--- app/src/main/java/com/alphaomardiallo/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/alphaomardiallo/go4lunch/ui/MainActivity.java
@@ -5,12 +5,17 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.alphaomardiallo.go4lunch.R;
 import com.alphaomardiallo.go4lunch.databinding.ActivityMainBinding;
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 9999;
     private ActivityMainBinding binding;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,31 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        startSignInActivityIfNotLogged();
+    }
+
+    private void startSignInActivityIfNotLogged() {
+        if (true) {
+            startSignInActivity();
+        }
+    }
+
+    private void startSignInActivity() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false, true)
+                        .setLockOrientation(true)
+                        .build(),
+                RC_SIGN_IN
+        );
     }
 }
