e266dae39f7f23ce52eec9c3ed6cc74a31d6982d
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-04-13 14:19:27 +0200

    response after sign in handled

--- app/src/main/java/com/alphaomardiallo/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/alphaomardiallo/go4lunch/ui/MainActivity.java
@@ -1,13 +1,20 @@
 package com.alphaomardiallo.go4lunch.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alphaomardiallo.go4lunch.R;
 import com.alphaomardiallo.go4lunch.databinding.ActivityMainBinding;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,13 +31,23 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        startSignInActivityIfNotLogged();
+        checkIfSignedIn();
     }
 
-    private void startSignInActivityIfNotLogged() {
+    private void checkIfSignedIn() {
         if (true) {
             startSignInActivity();
-        }
+        } /*else {
+                  Snackbar snackbar = Snackbar.make(binding.mainLayout, getString(R.string.You_are_connected), Snackbar.LENGTH_LONG)
+                    .setAction(getString(R.string.Disconnect), new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Snackbar snackbarBis = Snackbar.make(binding.mainLayout, "Message successfully disconnected.", Snackbar.LENGTH_SHORT);
+                            snackbarBis.show();
+                        }
+                    });
+            snackbar.show();
+        } */
     }
 
     private void startSignInActivity() {
@@ -49,5 +66,45 @@ public class MainActivity extends AppCompatActivity {
                         .build(),
                 RC_SIGN_IN
         );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    private void showSnackBar(String message) {
+        Snackbar.make(binding.mainLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // SUCCESS
+            if (resultCode == RESULT_OK) {
+                showSnackBar(getString(R.string.connection_succeed));
+            } else {
+                // ERRORS
+                if (response == null) {
+                    showSnackBar(getString(R.string.error_authentication_canceled));
+                } else if (response.getError() != null) {
+                    if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                        showSnackBar(getString(R.string.error_no_internet));
+                    } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                        showSnackBar(getString(R.string.error_unknown_error));
+                    }
+                }
+                final Handler handler = new Handler(Looper.getMainLooper());
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        checkIfSignedIn();
+                    }
+                }, 1000);
+            }
+        }
     }
 }
