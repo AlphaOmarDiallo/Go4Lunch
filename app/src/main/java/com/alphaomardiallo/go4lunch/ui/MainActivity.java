ad40a11f2e845cd8f2a08478ae3ae331f091996c
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-04-14 01:07:08 +0200

    deprecated method removed - checkIfSignedIn method implemented - Welcome Snackbar in Signed in - no more warnings

--- app/src/main/java/com/alphaomardiallo/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/alphaomardiallo/go4lunch/ui/MainActivity.java
@@ -1,28 +1,39 @@
 package com.alphaomardiallo.go4lunch.ui;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 
-import androidx.annotation.Nullable;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alphaomardiallo.go4lunch.R;
 import com.alphaomardiallo.go4lunch.databinding.ActivityMainBinding;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int RC_SIGN_IN = 9999;
     private ActivityMainBinding binding;
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            this::onSignInResult
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,69 +45,59 @@ public class MainActivity extends AppCompatActivity {
         checkIfSignedIn();
     }
 
-    private void checkIfSignedIn() {
-        if (true) {
-            startSignInActivity();
-        }
-    }
+    public void createSignInIntent() {
 
-    private void startSignInActivity() {
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.GoogleBuilder().build(),
                 new AuthUI.IdpConfig.TwitterBuilder().build(),
                 new AuthUI.IdpConfig.FacebookBuilder().build(),
-                new AuthUI.IdpConfig.EmailBuilder().build()
-        );
+                new AuthUI.IdpConfig.EmailBuilder().build());
 
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setTheme(R.style.LoginTheme)
-                        .setAvailableProviders(providers)
-                        .setIsSmartLockEnabled(false, true)
-                        .setLockOrientation(true)
-                        .build(),
-                RC_SIGN_IN
-        );
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setTheme(R.style.LoginTheme)
+                .setAvailableProviders(providers)
+                .setIsSmartLockEnabled(false, true)
+                .setLockOrientation(true)
+                .build();
+        signInLauncher.launch(signInIntent);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Log.e(TAG, "onSignInResult: user " + user, null);
+            showSnackBar(getString(R.string.connection_succeed));
+            // ...
+        } else {
+            // ERRORS
+            if (response == null) {
+                showSnackBar(getString(R.string.error_authentication_canceled));
+            } else if (response.getError() != null) {
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(getString(R.string.error_no_internet));
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(getString(R.string.error_unknown_error));
+                }
+            }
+            final Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(this::checkIfSignedIn, 1000);
+        }
+    }
+
+    private void checkIfSignedIn() {
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            createSignInIntent();
+        } else {
+            String userName = getString(R.string.welcome_back, FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+            showSnackBar(userName);
+        }
     }
 
     private void showSnackBar(String message) {
         Snackbar.make(binding.mainLayout, message, Snackbar.LENGTH_SHORT).show();
     }
 
-    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
-
-        IdpResponse response = IdpResponse.fromResultIntent(data);
-
-        if (requestCode == RC_SIGN_IN) {
-            // SUCCESS
-            if (resultCode == RESULT_OK) {
-                showSnackBar(getString(R.string.connection_succeed));
-            } else {
-                // ERRORS
-                if (response == null) {
-                    showSnackBar(getString(R.string.error_authentication_canceled));
-                } else if (response.getError() != null) {
-                    if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
-                        showSnackBar(getString(R.string.error_no_internet));
-                    } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
-                        showSnackBar(getString(R.string.error_unknown_error));
-                    }
-                }
-                final Handler handler = new Handler(Looper.getMainLooper());
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        checkIfSignedIn();
-                    }
-                }, 1000);
-            }
-        }
-    }
 }
