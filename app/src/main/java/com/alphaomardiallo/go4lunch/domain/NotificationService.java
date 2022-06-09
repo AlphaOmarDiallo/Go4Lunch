b7a8ce422cd45a480fbde4d6933f545030afe216
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-06-09 23:33:49 +0200

    Notification s'affiche - trouver une solution pour afficher el bon texte.

--- app/src/main/java/com/alphaomardiallo/go4lunch/domain/NotificationService.java
+++ app/src/main/java/com/alphaomardiallo/go4lunch/domain/NotificationService.java
@@ -1,24 +1,66 @@
 package com.alphaomardiallo.go4lunch.domain;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
+import com.alphaomardiallo.go4lunch.R;
+import com.alphaomardiallo.go4lunch.ui.MainActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class NotificationService extends FirebaseMessagingService {
 
     private static final String TAG = "NotificationService";
+    private final int NOTIFICATION_ID = 007;
+    private final String NOTIFICATION_TAG = "FIREBASEOC";
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         if (remoteMessage.getNotification() != null) {
-            // Get message sent by Firebase
             RemoteMessage.Notification notification = remoteMessage.getNotification();
-            Log.e(TAG, "onMessageReceived: " + notification.getBody(), null);
-            //sendVisualNotification(notification);
+            Log.e(TAG, "onMessageReceived: " + notification.getBody());
+            sendVisualNotification(notification);
         }
+    }
+
+    private void sendVisualNotification(RemoteMessage.Notification notification) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = getString(R.string.default_notification_channel_id);
+
+        // Build a Notification object
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.ic_baseline_ramen_dining_24)
+                        .setContentTitle(notification.getTitle())
+                        .setContentText(notification.getBody())
+                        .setAutoCancel(true)
+                        .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Support Version >= Android 8
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence channelName = "Firebase Messages";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(channelId, channelName, importance);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        // Show notification
+        notificationManager.notify(NOTIFICATION_TAG, NOTIFICATION_ID, notificationBuilder.build());
     }
 }
