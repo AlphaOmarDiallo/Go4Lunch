f219bef9f92189c8032482b2ca0833fe9ba968d2
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-06-10 12:04:07 +0200

    Notification is received even when app is is the background or not opened

---
+++ app/src/main/java/com/alphaomardiallo/go4lunch/domain/MyWorker.java
@@ -0,0 +1,20 @@
+package com.alphaomardiallo.go4lunch.domain;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class MyWorker extends Worker {
+    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        //TODO implement something to do
+        return null;
+    }
+}
--- app/src/main/java/com/alphaomardiallo/go4lunch/domain/NotificationService.java
+++ app/src/main/java/com/alphaomardiallo/go4lunch/domain/NotificationService.java
@@ -1,16 +1,21 @@
 package com.alphaomardiallo.go4lunch.domain;
 
+import static com.alphaomardiallo.go4lunch.MainApplication.CHANNEL_LUNCHTIME_REMINDER;
+
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
 import com.alphaomardiallo.go4lunch.R;
 import com.alphaomardiallo.go4lunch.ui.MainActivity;
@@ -21,29 +26,76 @@ public class NotificationService extends FirebaseMessagingService {
 
     private static final String TAG = "NotificationService";
     private final int NOTIFICATION_ID = 007;
-    private final String NOTIFICATION_TAG = "FIREBASEOC";
+    private final String NOTIFICATION_TAG = "GO4LUNCH";
+
+    public NotificationService() {
+    }
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        if (remoteMessage.getNotification() != null) {
-            RemoteMessage.Notification notification = remoteMessage.getNotification();
-            Log.e(TAG, "onMessageReceived: " + notification.getBody());
-            sendVisualNotification(notification);
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use WorkManager.
+                scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+                handleNow();
+            }
+
         }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        sendVisualNotification(remoteMessage.getNotification());
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+        sendRegistrationToServer(token);
+    }
+
+    private void scheduleJob() {
+        OneTimeWorkRequest work = new OneTimeWorkRequest.Builder(MyWorker.class).build();
+        WorkManager.getInstance(this).beginWith(work).enqueue();
+    }
+
+    private void handleNow() {
+        Log.d(TAG, "Short lived task is done.");
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
     }
 
     private void sendVisualNotification(RemoteMessage.Notification notification) {
 
         Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
-        String channelId = getString(R.string.default_notification_channel_id);
+        String channelId = CHANNEL_LUNCHTIME_REMINDER;
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
         // Build a Notification object
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, channelId)
                         .setSmallIcon(R.drawable.ic_baseline_ramen_dining_24)
+                        .setSound(defaultSoundUri)
                         .setContentTitle(notification.getTitle())
                         .setContentText(notification.getBody())
                         .setAutoCancel(true)
@@ -63,4 +115,5 @@ public class NotificationService extends FirebaseMessagingService {
         // Show notification
         notificationManager.notify(NOTIFICATION_TAG, NOTIFICATION_ID, notificationBuilder.build());
     }
+
 }
