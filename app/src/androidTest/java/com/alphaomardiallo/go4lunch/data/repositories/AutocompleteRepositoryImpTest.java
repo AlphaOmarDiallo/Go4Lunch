f4ee8ce4b9a90f1eb363d4118e31807f75d70640
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-06-15 20:38:11 +0200

    New tests

---
+++ app/src/androidTest/java/com/alphaomardiallo/go4lunch/data/repositories/AutocompleteRepositoryImpTest.java
@@ -0,0 +1,20 @@
+package com.alphaomardiallo.go4lunch.data.repositories;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AutocompleteRepositoryImpTest {
+
+    @Test
+    public void updatePredictionList() {
+    }
+
+    @Test
+    public void searchPredictionResults() {
+    }
+
+    @Test
+    public void setPlaceToFocusOn() {
+    }
+}
